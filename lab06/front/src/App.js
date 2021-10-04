b6175e63eb9bf1219baf48538b4605366b3f43a8
maxhha <devmaxhha@gmail.com>
2021-10-04 16:34:53 +0300

    move logic to App

--- lab06/front/src/App.js
+++ lab06/front/src/App.js
@@ -1,14 +1,54 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import Header from "./components/Header";
 import QueryCode from "./components/QueryCode";
+import useFetch from "use-http";
+import { API_URL } from "./constants";
 
 function App() {
   const [query, setQuery] = useState();
+  const [answer, setAnswer] = useState();
+
+  const queryInfo = useFetch(`${API_URL}/${query}?query=1`, {}, [query]);
+  const { get } = useFetch(API_URL);
+
+  const handleSelect = useCallback((query) => {
+    setQuery(query);
+    setAnswer(null);
+  }, []);
+
+  const handleClickLoad = useCallback(async () => {
+    const resp = await get(query);
+    setAnswer(resp);
+  }, [query]);
 
   return (
     <div>
-      <Header value={query} onSelect={setQuery} />
-      {query && <QueryCode queryName={query} />}
+      <Header value={query} onSelect={handleSelect} />
+      {query && queryInfo.loading && (
+        <span className="nes-text is-disabled">Загружается...</span>
+      )}
+      {query && queryInfo.error && (
+        <pre class="nes-text is-error">
+          {queryInfo.error.message || JSON.stringify(queryInfo.error, null, 2)}
+        </pre>
+      )}
+      {query && queryInfo.data?.query && (
+        <QueryCode code={queryInfo.data.query} />
+      )}
+      {query && queryInfo.data?.query && (
+        <button
+          class="nes-button"
+          disabled={queryInfo.data?.query == void 0}
+          onClick={handleClickLoad}
+        >
+          Запросить
+        </button>
+      )}
+      {answer && (
+        <pre className="nes-text is-dark">
+          {JSON.stringify(answer, null, 2)}
+        </pre>
+      )}
     </div>
   );
 }
